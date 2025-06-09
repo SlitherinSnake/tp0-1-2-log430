dcac15170a8c2e689fe7ee73bc0caece8130c128
Vu Minh <vu-minh.vu-le.1@ens.etsmtl.ca>
2025-06-09 03:52:45 -0400

    model

---
+++ lab2-magasin-web/src/main/java/com/log430/tp2/model/Produit.java
@@ -0,0 +1,95 @@
+package com.log430.tp2.model;
+
+import jakarta.persistence.*;
+
+// Annotation JPA indiquant que cette classe est une entité persistante
+@Entity
+// Nom de la table dans la base de données
+@Table(name = "produits")
+public class Produit {
+
+    // Identifiant unique pour chaque produit
+    @Id
+    // BD auto-génère les IDs
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nom;
+
+    private String categorie;
+
+    private float prix;
+
+    private int quantite;
+
+    // Constructeur par défaut requis par JPA
+    public Produit() {
+    }
+
+    // Constructeur avec paramètres pour instancier un produit
+    public Produit(String nom, String categorie, float prix, int quantite) {
+        this.nom = nom;
+        this.categorie = categorie;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+    // Methodes
+    public boolean hasStock(int qte) {
+        return quantite >= qte;
+    }
+
+    public void decreaseStock(int qte) {
+        if (!hasStock(qte))
+            throw new IllegalArgumentException("Stock insuffisant");
+        quantite -= qte;
+    }
+
+    // Getter/Setter
+    public int getId() {
+        return id;
+    }
+
+    // Pour test
+    public int setId(int id) {
+        return this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{id=" + id + ", nom='" + nom + "', categorie='" + categorie +
+                "', prix=" + prix + ", quantite=" + quantite + "}";
+    }
+}
---
+++ lab2-magasin-web/src/main/java/com/log430/tp2/model/RetourProduit.java
@@ -0,0 +1,81 @@
+package com.log430.tp2.model;
+
+import jakarta.persistence.*;
+
+// Annotation JPA indiquent que la classe est une entité persistante
+@Entity
+// Nom de la table dans la bd
+@Table(name = "retour_produit")
+public class RetourProduit {
+
+    // Identifiant unique pour chaque retour
+    @Id
+    // BD auto-génère les IDs
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    // Plusieurs retourProduit peuvent être liés à un seul retour
+    @ManyToOne
+    // Ceci précise que dans la table retourProduit, il y a une colonne retour_id
+    // pour identifier le retour.
+    @JoinColumn(name = "retour_id")
+    private Retour retour;
+
+    // Plusieurs retour de produit peuvent être effectués dans un retour
+    @ManyToOne
+    // Ceci précise que dans la table retourProduit, il y a une colonne produit_id
+    // pour identifier les produits.
+    @JoinColumn(name = "produit_id")
+    private Produit produit;
+
+    private int quantite;
+
+    // Constructeur par défaut requis par JPA
+    public RetourProduit() {
+
+    }
+
+    // Constructeur avec paramètres pour instancier un RetourProduit
+    public RetourProduit(Retour retour, Produit produit, int quantite) {
+        this.retour = retour;
+        this.produit = produit;
+        this.quantite = quantite;
+    }
+
+    // Getters / Setters
+    public int getId() {
+        return id;
+    }
+
+    public Retour getRetour() {
+        return retour;
+    }
+
+    public void setRetour(Retour retour) {
+        this.retour = retour;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    @Override
+    public String toString() {
+        return "RetourProduit{id=" + id +
+                ", produit=" + (produit != null ? produit.getNom() : "null") +
+                ", quantite=" + quantite + "}";
+    }
+
+}
