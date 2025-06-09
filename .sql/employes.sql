ae9d29b23111469a2977c5d348eb0c53a8643505
Vu Minh <vu-minh.vu-le.1@ens.etsmtl.ca>
2025-06-09 03:40:45 -0400

    lab1 sql

--- .sql/employes.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.employes (identifiant,nom) VALUES
-	 ('emp001','Alice'),
-	 ('emp002','Tom'),
-	 ('emp003','Paul'),
-	 ('emp004','Jennifer');
---
+++ lab1-magasin-cli/.sql/employes.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.employes (identifiant,nom) VALUES
+	 ('emp001','Alice'),
+	 ('emp002','Tom'),
+	 ('emp003','Paul'),
+	 ('emp004','Jennifer');
