17ab1df7c4a3bd84841a807c1d58125119c7bbb7
Vu Minh <vu-minh.vu-le.1@ens.etsmtl.ca>
2025-07-04 23:25:27 -0400

    New launcher

---
+++ lab4-magasin-api-rest-advanced/src/main/java/com/log430/tp4/MagasinApiApplication.java
@@ -0,0 +1,18 @@
+package com.log430.tp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Main Spring Boot application class for the Magasin API.
+ * Follows DDD architecture with domain-driven design principles.
+ */
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.log430.tp4.infrastructure.repository")
+public class MagasinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagasinApiApplication.class, args);
+    }
+}
