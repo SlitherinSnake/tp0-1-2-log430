91bb25fe0fdb4e60fe7ff8190175b2e2b4e44618
Vu Minh <vu-minh.vu-le.1@ens.etsmtl.ca>
2025-07-11 01:22:41 -0400

    discoveryservice microservice

---
+++ lab5-magasin-api-microservice/discovery-server/src/main/java/com/log430/tp5/discovery/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.log430.tp5.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
